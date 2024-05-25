634ebfbaff2cdce7989bdc5dd934597afdb4a0a5
rznnakdavila <ruan.bueno70@gmail.com>
2024-05-25 18:01:31 -0300

    Ultimas Alterações

--- CepAPI/CepAPI/Pages/Login.cshtml.cs
+++ CepAPI/CepAPI/Pages/Login.cshtml.cs
@@ -30,7 +30,7 @@ namespace CepAPI.Pages
             {
                 if (Model.Email == "admin@aec.com.br" && Model.Password == "Ab!123")
                 {
-                    return RedirectToPage("Admin");
+                    return RedirectToPage("Error");
                 }
 
                 var user = await userManager.FindByEmailAsync(Model.Email);
--- CepAPI/CepAPI/obj/Debug/net8.0/CepAPI.AssemblyInfo.cs
+++ CepAPI/CepAPI/obj/Debug/net8.0/CepAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CepAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763d635629d28eed529be99f2030fa4d9ceb473f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9949007ce3be4880888019c29ced0df758b5af69")]
 [assembly: System.Reflection.AssemblyProductAttribute("CepAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CepAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
